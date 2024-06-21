d5a48ec276ba8bc4f43e92260d8b4325787ce241
Anup Singh <96727427+NIAnup@users.noreply.github.com>
2024-06-21 14:35:57 +0530

    new project

---
+++ qrcode/main-files/qrpay-user-app/qrpay-user-app-new-v4.4.0/lib/res/assets_res.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String PLUGIN_NAME = 'qrpay';
+  static const String PLUGIN_VERSION = '4.2.0+14';
+  static const String AGENT = 'assets/icon/agent.svg';
+}
